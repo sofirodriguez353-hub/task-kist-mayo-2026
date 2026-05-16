21b55291b16de668ddf61e5055815c9d4a60698b
SOFIA RODRIGUEZ <sofirodriguez353@gmail.com>
2026-05-16 17:31:56 -0400

    task detail is done

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListView 
+from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
+    path('tareas/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from .models import Task
 # Create your views here.
 
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
+
+class TaskDetailView(DeleteView):
+    model = Task
+    template_name = 'task_detail.html'
